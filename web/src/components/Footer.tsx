42ad15a7144b00583d5b78a1a204585410fdb203
madhusudhant03 <madhusudhant307@gmail.com>
2026-05-04 04:53:50 -0700

    Update Google Maps location to Trek Group Business Services in Doha

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -110,7 +110,7 @@ export default function Footer() {
             
             <div className="rounded-2xl overflow-hidden border border-gray-100 shadow-lg w-full h-48 md:h-56">
               <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3608.1888981961474!2d51.5284149!3d25.2644211!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e45c534ffdce87f%3A0x1cfa88cf812b4032!2sDoha%2C%20Qatar!5e0!3m2!1sen!2sus!4v1714000000000!5m2!1sen!2sus" 
+                src="https://maps.google.com/maps?q=TREK%20GROUP%20BUSINESS%20SERVICES%20Nuaija%2043%20Doha%20Qatar&t=&z=15&ie=UTF8&iwloc=&output=embed" 
                 width="100%" 
                 height="100%" 
                 style={{border:0}} 
